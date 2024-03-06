403f0280cb30b63f1217004864f722b0d6e1ec5b
jasminewalid <jasmine.walid.mohamed@gmail.com>
2024-03-06 18:48:28 +0200

    fixed the error

--- main.sh
+++ main.sh
@@ -1,32 +1,27 @@
-function main-menu
-{
-    echo -e "    +==============================================================+"
-    echo -e "    |                       Main Menu                              |"
-    echo -e "    |==============================================================|"
-    echo -e "    |                    1.Create Database                         |"     
-    echo -e "    |                    2.List Databases                          |"    
-    echo -e "    |                    3.Connect To Databases                    |"    
-    echo -e "    |                    4.Drop Database                           |"    
-    echo -e "    |                    5.Exit                                    |"
-    echo -e "    +==============================================================+"
+#!/bin/bash
+
+main_menu() {
+    echo -e "+==============================================================+"
+    echo -e "|                       Main Menu                              |"
+    echo -e "|==============================================================|"
+    echo -e "|                    1. Create Database                       |"     
+    echo -e "|                    2. List Databases                        |"    
+    echo -e "|                    3. Connect To Databases                  |"    
+    echo -e "|                    4. Drop Database                         |"    
+    echo -e "|                    5. Exit                                  |"
+    echo -e "+==============================================================+"
     echo -e "Please choose a number : " 
 
-    read userChoice
-    case userChoice in
-
-        1) main_menu/createDB.sh;;
-
-        2) main_menu/listDB.sh;;
-
-        3) main_menu/connectToDB.sh;;
-
-        4) main_menu/dropDB.sh;;
-
-        5) echo -e "ByeBye!";
-		exit;;
-
-        *) echo -e "invalid choice, please choose from the listed items!";
-	main-menu 
+    read -r userChoice
+    case $userChoice in
+        1) main_menu/createDB.sh ;;
+        2) main_menu/listDB.sh ;;
+        3) main_menu/connectToDB.sh ;;
+        4) main_menu/dropDB.sh ;;
+        5) echo -e "ByeBye!"
+           exit ;;
+        *) echo -e "Invalid choice, please choose from the listed items!"
+           main_menu ;;
     esac
 }
-main-menu
+main_menu
