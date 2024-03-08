512444e58a31b4f1c7105bda32272f29c30ac611
jasminewalid <jasmine.walid.mohamed@gmail.com>
2024-03-08 03:27:23 +0200

    added rows

--- table_menu/Insert-Into-Table.sh
+++ table_menu/Insert-Into-Table.sh
@@ -3,69 +3,65 @@
 echo "Enter your Database Name : "
 read DB_name
 
-echo "Avilable tables are : "`ls ./databases/$DB_name`
+echo "Available tables are : $(ls ./databases/$DB_name)"
 
-echo "Which table you want to insert into : "
+echo "Which table do you want to insert into : "
 read TableName
 
-row="";
-coloumnsCount=`awk -F, 'NR==1 {print NF}' ./databases/$DB_name/$TableName`
+row=""
 
-for(( i=1;i<$coloumnsCount;i++ ))
-do
-	columnNumber=$(echo $i|cut -d ',' -f$i) ;
-	echo -n $i "- "
-	
-	columnType=`grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | cut -d% -f3`  
-	colName=`grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | cut -d% -f2`  
+# Get the number of columns in the table
+columnCount=$(awk -F, 'NR==1 {print NF}' ./databases/$DB_name/$TableName)
 
-	echo "Column Name : $colName , it's Type : $columnType" 
-	
-	testPk=`grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | grep %Pk% | cut -d% -f4` 
-	
-	echo -n "Enter the value of column number $columnNumber : ";
-	read  val;
+for ((i=1; i<=$columnCount; i++)); do
+    columnNumber=$i
+    echo -n "$i - "
 
-	#validation for string datatype
-	if test $columnType = "string"
-	then
-		while [ -z $val ] 2>/dev/null || [[ $val =~ [\,\;\:\-\/\\] ]] || [ "$val" -eq "$val" ] 2>/dev/null
-		do
-			echo "Invalid input!, empty value or special characters are not allowed";
-			echo -n "Enter the value of column number $columnNumber : ";
-			read  val;
-		done
-	fi
+    # Get column type and name
+    columnType=$(grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | cut -d% -f3)
+    colName=$(grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | cut -d% -f2)
 
-	# Validation for integer datatype
-	if test $columnType = "int"
-	then
-		while ! [ "$val" -eq "$val" ] 2>/dev/null
-		do
-			echo "Invalid input!, datatype of column number $columnNumber is integer";
-			echo -n "Enter the value of column number $columnNumber : ";
-			read  val;
-		done
-	fi
+    echo "Column Name: $colName, Type: $columnType"
 
-	#appending row to table with "," as delimeter
-	if testPk=='Pk'
-	then
-		if checkNewValue=`cut -f$columnNumber -d, ./databases/$DB_name/$TableName | grep -w $val`
-		then 
-			echo "this value exit"
-			main_menu/connectToDB.sh 
-		else
-			if test -z $row
-			then
-				row=$val
-				else
-					row=$row","$val;
-			fi
-		fi
-	fi
+    # Prompt user for value of the column
+    echo -n "Enter the value of column number $columnNumber : "
+    read val
 
+    # Validate input based on column data type
+    if [ "$columnType" == "string" ]; then
+        # Validation for string datatype
+        while [[ -z $val || $val =~ [\,\;\:\-\/\\] ]]; do
+            echo "Invalid input! Empty value or special characters are not allowed."
+            echo -n "Enter the value of column number $columnNumber : "
+            read val
+        done
+    elif [ "$columnType" == "int" ]; then
+        # Validation for integer datatype
+        while ! [[ "$val" =~ ^[0-9]+$ ]]; do
+            echo "Invalid input! Datatype of column number $columnNumber is integer."
+            echo -n "Enter the value of column number $columnNumber : "
+            read val
+        done
+    fi
+
+    # Check if the column is a primary key
+    testPk=$(grep "%," ./databases/$DB_name/$TableName | cut -d "," -f$columnNumber | grep %Pk% | cut -d% -f4)
+
+    # Check if the value already exists in the primary key column
+    if [ "$testPk" == "Pk" ]; then
+        if grep -qw "$val" ./databases/$DB_name/$TableName; then
+            echo "This value already exists"
+            main_menu/connectToDB.sh
+        else
+            if [ -z "$row" ]; then
+                row="$val"
+            else
+                row="$row,$val"
+            fi
+        fi
+    fi
 done
-echo "$row" >> ./databases/$DB_name/$TableName 2> /dev/null
-echo "Your Data Updated Sucessfuly"
-main_menu/connectToDB.sh 
+
+echo "$row" >> ./databases/$DB_name/$TableName 2>/dev/null
+echo "Your data has been updated successfully"
+main_menu/connectToDB.sh
